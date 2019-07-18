336aba5e7f9252e671c9c3345410875699a83643
Thobanjalo Shangase <thoba@bluwave.co.za>
2019-07-18 15:38:46 +0200

    Initial working files

---
+++ app.vue
@@ -0,0 +1,17 @@
+<!-- Development version with useful console warnings -->
+<script src = "https://cdn.jsdelivr.net/npm/vue/dist/vue.js" ></script>
+
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+    var app = new Vue({
+        el: '#app',
+        data: {
+          message: 'Hello Vue!'
+        }
+    })
+</script>
